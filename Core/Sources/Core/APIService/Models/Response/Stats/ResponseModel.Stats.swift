ca23625afaba87dcaa1bd9400925482a7672c0e5
Daniel Astine <daniel@astine.com.br>
2019-09-23 20:45:49 -0300

    Create Stats response model

---
+++ Core/Sources/Core/APIService/Models/Response/Stats/ResponseModel.Stats.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public extension ResponseModel {
+    struct Stats {
+        let date: Date
+        let marketPriceInUSD: Decimal
+    }
+}
+
+// MARK: - Stats Decodable extension
+extension ResponseModel.Stats: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let marketPriceInUSD: Decimal = try container.decode(Decimal.self, forKey: .marketPriceInUSD)
+
+        let dateTimestamp: TimeInterval = try container.decode(TimeInterval.self, forKey: .timestamp)
+        let date = Date(timeIntervalSince1970: dateTimestamp)
+
+        self.init(date: date, marketPriceInUSD: marketPriceInUSD)
+    }
+}
+
+private extension ResponseModel.Stats {
+    enum CodingKeys: String, CodingKey {
+        case timestamp = "timestamp"
+        case marketPriceInUSD = "market_price_usd"
+    }
+}
